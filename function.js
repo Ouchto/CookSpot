df7d9c033fca3aeb3b353fd7b22c0798ea636584
kawtarmouslim <kawtharmouslim33@gmail.com>
2025-01-19 19:42:18 +0100

    modification

--- function.js
+++ function.js
@@ -15,7 +15,6 @@ function loadRecipes() {
 
 
 
-
 function renderRecipes(recipes) {
     recipeList.innerHTML = recipes.map((recipe, index) => `
         <div style="width: 100%; max-width: 300px; margin: 20px auto; border: 1px solid #ddd; border-radius: 8px; overflow: hidden; background-color: #fff; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); text-align: center;">
